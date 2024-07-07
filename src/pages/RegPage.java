ac4052676f864e1f31976f449bf7750c1d1e1bf0
im-vidhyashankar <vinutesting7@gmail.com>
2024-07-07 15:51:30 +1000

    Updated MyTest classes with city

--- src/pages/RegPage.java
+++ src/pages/RegPage.java
@@ -1,5 +1,8 @@
 package pages;
 
 public class RegPage {
+	
+	int i = 10;
+	String d = "Shankar";
 
 }
--- src/tests/MyTest.java
+++ src/tests/MyTest.java
@@ -5,4 +5,5 @@ public class MyTest {
 	String name ="VidhyaTest";
 	int age = 25;
 	String city = "Australia";
+	int salary =100;
 }
